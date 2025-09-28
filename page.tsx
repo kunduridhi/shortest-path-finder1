4fe6ae04eb59bfdf5a0e26b0aaed14b9e20fa525
Ridhi Kundu <ridhi.kundu2024@vitstudent.ac.in>
2025-09-28 20:13:51 +0600

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import PathfinderVisualizer from "@/components/pathfinder-visualizer"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <PathfinderVisualizer />
+    </main>
+  )
+}
